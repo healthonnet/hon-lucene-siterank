c191ae8e064b5a6401d80f2ccfdd566ed8050be4
Nolan Lawson <nolan.lawson@gmail.com>
2012-11-05 16:06:10 +0100

    fix unit tests

--- src/test/java/org/healthonnet/lucene/siterank/SiteRankTest.java
+++ src/test/java/org/healthonnet/lucene/siterank/SiteRankTest.java
@@ -1,5 +1,6 @@
 package org.healthonnet.lucene.siterank;
 
+import org.healthonnet.lucene.siterank.source.SiteRankSource;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,7 +12,16 @@ public class SiteRankTest {
         Assert.assertEquals(430, new MockSiteRankSource().getRankInfo("webmd.com").getRank());
         Assert.assertEquals(4706956, new MockSiteRankSource().getRankInfo("thebuttercompartment.com").getRank());
         
-        // no foobar.com
-        Assert.assertFalse(new MockSiteRankSource().getRankInfo("foobar.com").isSiteFound());
+    }
+    
+    @Test
+    public void testSiteRankWithHostParsing() {
+        SiteRankSource siteRankSource = new MockSiteRankSource();
+        siteRankSource.setExtractDomainFromUrl(true);
+        
+        Assert.assertEquals(1, siteRankSource.getRankInfo("http://google.com/mail").getRank());
+        Assert.assertEquals(430, siteRankSource.getRankInfo("http://webmd.com").getRank());
+        Assert.assertEquals(4706956, siteRankSource.getRankInfo("http://thebuttercompartment.com/some/page").getRank());
+        
     }
 }
